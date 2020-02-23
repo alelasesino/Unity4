492be1db790f83fd2ece04787ca6d0c1d5062cc3
ALEJANDRO <ALEJANDRO@DESKTOP-8QQCSOH>
2020-02-23 17:43:28 +0100

    Move Player And IA Works!

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+
+    [Range(2.5f, 5f)]
+    public float visionRadius;
+
+    [Range(1f, 2f)]
+    public float speed;
+
+    public GameObject player;
+
+    private Vector3 initialPosition;
+
+    void Start()
+    {
+        initialPosition = transform.position;
+    }
+
+    void Update()
+    {
+        MoveEnemy();
+    }
+
+    private void MoveEnemy(){
+    
+        Vector3 target = initialPosition;
+
+        float distance = Vector3.Distance(player.transform.position, transform.position);
+        if(distance < visionRadius) target = player.transform.position;
+
+        float fixedSpeed = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, target, fixedSpeed);
+
+        Debug.DrawLine(transform.position, target, Color.red);
+
+    }
+
+    private void OnDrawGizmos() {
+        
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, visionRadius);
+
+    }
+
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    
+    //Posicion de destino del jugador
+    private Vector3 target;
+    
+    [Range(3f, 5f)]
+    public float speed = 2f;
+
+    private const int LEFT_CLICK = 0;
+
+    void Start()
+    {
+        target = transform.position;
+    }
+
+    void Update()
+    {
+        MovePlayer();
+    }
+
+    private void MovePlayer(){
+    
+        if(Input.GetMouseButtonDown(LEFT_CLICK)){
+        
+            target = Camera.main.ScreenToWorldPoint(Input.mousePosition);    
+            target.z = 0f;
+
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, target, speed*Time.deltaTime);
+        Debug.DrawLine(transform.position, target, Color.green);
+
+    }
+
+}
